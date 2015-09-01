06ac13898cebb5b8fd4efe212776da8d5795e6a7
Akash Chaturvedi <axc144430@utdallas.edu>
2015-08-31 21:05:49 -0500

    more layout changes in HomeActivity, launch Home activity on app launch now

--- app/src/main/java/com/remindme/android/remindme/HomeActivity.java
+++ app/src/main/java/com/remindme/android/remindme/HomeActivity.java
@@ -8,10 +8,12 @@ import android.view.MenuItem;
 
 public class HomeActivity extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 
     @Override
